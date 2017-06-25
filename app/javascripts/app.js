c98367a47ac4d45e92aefef543ee9a73ef90d443
Shivam Dawer <shivamdawer.72@gmail.com>
2017-06-26 01:47:01 +0530

    changes

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -175,47 +175,40 @@ function updateElement(id, value) {
   ele.innerHTML = value;
 }
 
-function decode_base64 (s)
-{
-    var e = {}, i, k, v = [], r = '', w = String.fromCharCode;
-    var n = [[65, 91], [97, 123], [48, 58], [43, 44], [47, 48]];
+function decode_base64 (s) {
+  var e = {}, i, k, v = [], r = '', w = String.fromCharCode;
+  var n = [[65, 91], [97, 123], [48, 58], [43, 44], [47, 48]];
 
-    for (z in n)
-    {
-        for (i = n[z][0]; i < n[z][1]; i++)
-        {
-            v.push(w(i));
-        }
+  for (z in n) {
+    for (i = n[z][0]; i < n[z][1]; i++) {
+      v.push(w(i));
     }
-    for (i = 0; i < 64; i++)
-    {
-        e[v[i]] = i;
-    }
+  }
+  for (i = 0; i < 64; i++) {
+    e[v[i]] = i;
+  }
 
-    for (i = 0; i < s.length; i+=72)
-    {
-        var b = 0, c, x, l = 0, o = s.substring(i, i+72);
-        for (x = 0; x < o.length; x++)
-        {
-            c = e[o.charAt(x)];
-            b = (b << 6) + c;
-            l += 6;
-            while (l >= 8)
-            {
-                r += w((b >>> (l -= 8)) % 256);
-            }
-         }
+  for (i = 0; i < s.length; i+=72) {
+    var b = 0, c, x, l = 0, o = s.substring(i, i+72);
+    for (x = 0; x < o.length; x++) {
+      c = e[o.charAt(x)];
+      b = (b << 6) + c;
+      l += 6;
+      while (l >= 8) {
+        r += w((b >>> (l -= 8)) % 256);
+      }
     }
-    return r;
+  }
+  return r;
 }
 
-function generateRandomString(length = 10)
-{
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+function generateRandomString(length = 10) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < length; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for( var i=0; i < length; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
-    return text;
+  return text;
 }
